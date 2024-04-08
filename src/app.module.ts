38ae57721210c52fbab53710e1647f2a992be884
nhat <nhat1234559@gmail.com>
2024-04-08 21:25:23 +0700

    feat: setup codebase , swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { TagsModule } from './tags/tags.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [ArticlesModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SharedModule,
+    ArticlesModule,
+    UsersModule,
+    AuthModule,
+    CommentModule,
+    TagsModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from '~/app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import {
+  swaggerPath,
+  swaggerDocumentOptions,
+  swaggerSetupOptions,
+} from "~/shared/swagger/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  
+  const document = SwaggerModule.createDocument(app, swaggerDocumentOptions);
+  SwaggerModule.setup(swaggerPath, app, document, swaggerSetupOptions);
   await app.listen(3000);
 }
 bootstrap();
