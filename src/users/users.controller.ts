7826f55e32dd1bf78456f502933b350f57b162bf
Sơn <88760761+ahihison@users.noreply.github.com>
2024-04-15 07:08:54 +0700

    feat: api user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -10,22 +10,32 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto.username, createUserDto.password);
+  create(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto>{
+    
+    return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  findByEmail(@Query('email') email: string): Promise<CreateUserDto>{
+   
+    return this.usersService.findByEmail(email);
+  } 
+
+  @Get('all')
+  findAll(): Promise<CreateUserDto[]>{
+   
     return this.usersService.findAll();
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto)
+  :Promise<UpdateUserDto> {
+
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
