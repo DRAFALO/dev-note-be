f92ee3c8c61f15002a089f0d4dae916ab1b9ff88
Sơn <88760761+ahihison@users.noreply.github.com>
2024-04-22 09:03:41 +0700

    feat: api commnet

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,36 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { ApiTags } from '@nestjs/swagger';
-
+import {Comment} from './schema/comment.schema';
+import { Response } from 'express';
 @ApiTags('Comment')
 @Controller('comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
+  create(@Body() createCommentDto: CreateCommentDto):Promise<Comment> {
     return this.commentService.create(createCommentDto);
   }
 
-  @Get()
+  @Get('all')
   findAll() {
     return this.commentService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
+  findOne(@Param('id') id: string):Promise<Comment> {
+    return this.commentService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
+    return this.commentService.update(id, updateCommentDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    const result = await this.commentService.remove(id);
+    if (result === null) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+    return res.status(200).json({ message: 'Comment removed successfully' });
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, isValidObjectId } from 'mongoose';
+import { Comment,CommentDocument } from './schema/comment.schema';
 
 @Injectable()
 export class CommentService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(@InjectModel(Comment.name) private readonly commentModel: Model<CommentDocument>) { }
+
+  async create(createCommentDto: CreateCommentDto):Promise<Comment> {
+    const newComment = new this.commentModel(createCommentDto);
+    newComment.createdAt = new Date();
+    newComment.updatedAt = new Date();
+    return newComment.save()
   }
 
-  findAll() {
-    return `This action returns all comment`;
+  async findAll():Promise<Comment[]> {
+    return this.commentModel.find().exec()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  async findOne(id: string):Promise<Comment> {
+    const _id = new Object(id); 
+    return this.commentModel.findOne({ _id}).exec();
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(id: string, updateCommentDto: UpdateCommentDto) {
+    const _id = new Object(id);
+    const {content, likesCount, specialLike} = updateCommentDto;
+    //check author ID === user ID 
+
+    const dataUpdate = {
+      ...updateCommentDto,
+      content: content,
+      likesCount: likesCount,
+      specialLike: specialLike,
+      updatedAt: new Date(),
+     
+    }
+    const updatedComment = await this.commentModel.findOneAndUpdate({_id}, dataUpdate,{ new: true })
+    return updatedComment;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(id: string) {
+    if (!isValidObjectId(id)) {
+      return null;
+    }
+    const _id = new Object(id);
+    const comment = await this.commentModel.findOne({ _id }).exec();
+    if (!comment) {
+      return null;
+    }
+    return this.commentModel.deleteOne().where({ _id }).exec();
   }
 }
