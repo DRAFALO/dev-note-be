ea70f610a37d686c148226c8f48caab03d0b7745
Sơn <88760761+ahihison@users.noreply.github.com>
2024-04-22 07:19:45 +0700

    feat: article api crud

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -12,27 +12,34 @@ export class ArticlesService {
     @InjectModel(Article.name) private articleModel: Model<ArticleDocument>
   ) { }
 
-  create(createArticleDto: CreateArticleDto){
-
-    return 'This action adds a new article';
+  async create(createArticleDto: CreateArticleDto):Promise<Article>{
+    const newArticle = new this.articleModel(createArticleDto);
+    newArticle.createAt = new Date().toISOString();
+    newArticle.updateAt = new Date().toISOString();
+    console.log(newArticle);
+    return newArticle.save();
   }
 
-  async findAll() {
+  async findAll():Promise<Article[]> {
     const article = await this.articleModel.find({});
     return article;
   }
 
-  async findOne(id: number) {
 
-    return `This action returns a #${id} article`;
+  async findOne(id: string): Promise<Article> {
+    const _id = new Object(id);
+    const article = this.articleModel.findOne({_id});
+    return article;
+  }
+ 
+  async update(id: string, updateArticleDto: UpdateArticleDto):Promise<Article> {
+    const _id = new Object(id);
+    const updatedArticle = await this.articleModel.findOneAndUpdate({_id}, updateArticleDto,{ new: true })
+    return updatedArticle;
   }
 
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-
-    return `This action updates a #${id} article`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} article`;
+  remove(id: string) {
+    const _id = new Object(id);
+    return this.articleModel.deleteOne().where({ _id }).exec();
   }
 }
--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,16 @@
-export class CreateArticleDto {}
+import { TypeArticle } from "../schema/article.schema";
+
+export class CreateArticleDto {
+  title: string;
+  banner: string;
+  content: string;
+  type: TypeArticle;
+  emotion: string[];
+  tag: number[];
+  watchLater: boolean;
+  author: string;
+  createAt: string; 
+  updateAt: string;
+  
+
+}
