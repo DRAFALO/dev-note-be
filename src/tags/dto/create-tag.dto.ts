9b4c50499cce88fd7ee7b84f2540633b9930f7b8
Minh Trường <letruong147123@gmail.com>
2024-04-23 18:14:46 +0700

    [XByYq8vz]: CRUD Tag

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTagDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length } from 'class-validator';
+export class CreateTagDto {
+  @ApiProperty({
+    description: 'The label of the tag',
+    minLength: 6,
+    maxLength: 50,
+    required: true,
+    type: String,
+  })
+  @IsString()
+  @Length(6, 50)
+  label: string;
+}
--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,36 +1,98 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TagsService } from './tags.service';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateTagDto } from './dto/create-tag.dto';
+import { Tag } from './schema/tag.schema';
+import { TagsService } from './tags.service';
 import { UpdateTagDto } from './dto/update-tag.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 
 @ApiTags('Tag')
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
+  @ApiOperation({ summary: 'Create a new tag' })
+  @ApiResponse({
+    status: 201,
+    description: 'Create success.',
+  })
+  @ApiBody({ type: CreateTagDto })
   @Post()
-  create(@Body() createTagDto: CreateTagDto) {
+  async create(@Body() createTagDto: CreateTagDto): Promise<Tag> {
     return this.tagsService.create(createTagDto);
   }
 
+  @ApiOperation({ summary: 'Get all tags' })
+  @ApiResponse({
+    status: 200,
+    description: 'Find all successfully.',
+  })
   @Get()
   findAll() {
     return this.tagsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tagsService.findOne(+id);
+  @ApiOperation({ summary: 'Search tags' })
+  @ApiResponse({
+    status: 200,
+    description: 'Find successfully.',
+  })
+  @ApiQuery({
+    name: 'label',
+    required: true,
+    type: String,
+    description: 'Search label for tags',
+  })
+  @Get('search')
+  findByTag(@Query('label') label: string) {
+    return this.tagsService.findByNameTag(label);
   }
 
+  @ApiOperation({ summary: 'Get tag by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Find by ID successfully.',
+  })
+  @ApiParam({ name: 'id', type: 'string' })
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.tagsService.findById(id);
+  }
+
+  @ApiOperation({ summary: 'Update a tag' })
+  @ApiResponse({
+    status: 200,
+    description: 'Update successfully.',
+  })
+  @ApiBody({ type: CreateTagDto })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
-    return this.tagsService.update(+id, updateTagDto);
+    return this.tagsService.update(id, updateTagDto);
   }
 
+  @ApiOperation({ summary: 'Delete a tag' })
+  @ApiResponse({
+    status: 200,
+    description: 'Delete success.',
+  })
+  @ApiParam({ name: 'id', type: 'string' })
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.tagsService.remove(+id);
+    return this.tagsService.remove(id);
   }
 }
