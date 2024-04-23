654d7dea69493fb4a7adee9d39e1139046f9a6ff
nhat <nhat1234559@gmail.com>
2024-04-23 22:17:07 +0700

    fix: hotfix when create new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,35 +7,38 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  
+
 
   constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) { }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const { username, password,email } = createUserDto;
-    const newUser:User = {
-      username,
-      password,
-      role: UserRole.User,
-      email: email,
-      firstName: '',
-      lastName: '',
-      birth: undefined,
-      avatar: '',
-      createdAt: undefined,
-      updatedAt: undefined,
-      follower: undefined,
-      following: undefined,
-      social_link: '',
-      location: undefined,
-      skills: [],
-      tag_following: ''
-    }
+    const { username, password, email } = createUserDto;
+   
+    const newUser = new User();
+    newUser.username = username;
+    newUser.password = password;
+    newUser.email = email;
+    newUser.role = UserRole.User;
+    newUser.isRegisteredWithGoogle = false;
+    newUser.firstName = '';
+    newUser.lastName = '';
+    newUser.birth = undefined;
+    newUser.avatar = '';
+    newUser.createdAt = undefined;
+    newUser.updatedAt = undefined;
+    newUser.follower = undefined;
+    newUser.following = undefined;
+    newUser.social_link = '';
+    newUser.location = undefined;
+    newUser.skills = [];
+    newUser.tag_following = '';
+
+
     const checkUserExist = await this.findByEmail(email);
     if (checkUserExist) {
       throw new BadRequestException('User already exist');
     }
-    const createdUser  = await this.userModel.create(newUser);
+    const createdUser = await this.userModel.create(newUser);
     const userResponse: User = createdUser.toObject();
     delete userResponse.password;
     return userResponse;
@@ -45,19 +48,19 @@ export class UsersService {
     return this.userModel.findOne(query);
   }
 
-  async findAll(): Promise<User[]>{
+  async findAll(): Promise<User[]> {
 
     return this.userModel.find().exec();
   }
 
   async findByEmail(email: string): Promise<User> {
-    const user = this.userModel.findOne({email})
+    const user = this.userModel.findOne({ email })
     return user;
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-   const _id = new Object(id);
-    const updatedUser = await this.userModel.findOneAndUpdate({_id}, updateUserDto,{ new: true })
+    const _id = new Object(id);
+    const updatedUser = await this.userModel.findOneAndUpdate({ _id }, updateUserDto, { new: true })
     return updatedUser;
   }
 
